486e05b986086ff40dc91649ebf3abad68c83774
Jeifer Martinez <100806826+Otar91@users.noreply.github.com>
2023-05-23 15:58:07 -0500

    "Agregando un animal adicional para probar git"

--- 3. Lists/list_pizza_exercise.py
+++ 3. Lists/list_pizza_exercise.py
@@ -1,18 +1,18 @@
-pizzas=['queso  y jamon','pollo y champinon','solo carne'];
-#for loop just names pizzas
+pizzas = ["queso  y jamon", "pollo y champinon", "solo carne"]
+# for loop just names pizzas
 for pizza in pizzas:
-    print(pizza.upper());
+    print(pizza.upper())
 
-#for loop to print each name in the list with  a phrase
+# for loop to print each name in the list with  a phrase
 for pizza in pizzas:
-    print("A samuel le encanta la pizza de "+pizza.title());
-print('Actually Sami really love eat pizza and his coke of course');
+    print("A samuel le encanta la pizza de " + pizza.title())
+print("Actually Sami really love eat pizza and his coke of course")
 
-#animals lists
-animals=['bird','duck','raven'];
+# animals lists
+animals = ["bird", "duck", "raven", "lion"]
 for animal in animals:
-    print(animal.title());
+    print(animal.title())
 
 for animal in animals:
-    print('This '+ animal +' will be a great pet');
-print('All this animals have wings');
+    print("This " + animal + " will be a great pet")
+print("All this animals have wings")
